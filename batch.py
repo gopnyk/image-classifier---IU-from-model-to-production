31791a4688fd6a9caf101435feb081fc78e945d6
gopnyk <ugniusvisk@gmail.com>
2026-06-09 14:17:42 +0300

    changing the python version and tenserflow version

--- batch.py
+++ batch.py
@@ -12,8 +12,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 # Load the model
-model = tf.keras.models.load_model("models/image_classifier.keras", safe_mode= False,
-                                   custom_objects={"quantization_config": None})
+model = tf.keras.models.load_model("models/image_classifier.keras")
 
 # All categories within the model
 class_names = ['beauty_products', 'electronics', 'fashion', 'fitness_equipments', 'furniture',
